b66d6212cfac5d87b883c732b7d442173f5e069d
Sirisha111102 <84067879+Sirisha111102@users.noreply.github.com>
2021-05-29 11:36:59 +0530

    Add files via upload

---
+++ reversearray.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main() 
+{
+	int n,arr[n];
+	cin>>n;
+	for(int i=0;i<n;i++)
+	{
+	    cin>>arr[i];
+	}
+	int temp;
+	for(int i=0;i<n/2;i++)
+	{
+	    temp=arr[i];
+	    arr[i]=arr[n-i-1];
+	    arr[n-i-1]=temp;
+	}
+	cout<<"the reversed array is:"<<endl;
+	for(int i=0;i<n;i++)
+	{
+	    cout<<arr[i]<<" ";
+	}
+	return 0;
+}
